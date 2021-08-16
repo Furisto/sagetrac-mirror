bcd861e0cdae608cffdc4344a84099d278255634
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-08-15 18:11:35 -0700

    sage.interfaces.maxima_lib: Repair conversion of MEQUAL, MNOTEQUAL to sage

--- src/sage/interfaces/maxima_lib.py
+++ src/sage/interfaces/maxima_lib.py
@@ -229,6 +229,7 @@ meval=EclObject("MEVAL")
 msetq=EclObject("MSETQ")
 mlist=EclObject("MLIST")
 mequal=EclObject("MEQUAL")
+mnotequal=EclObject("MNOTEQUAL")
 cadadr=EclObject("CADADR")
 
 max_integrate=EclObject("$INTEGRATE")
@@ -1227,6 +1228,9 @@ max_op_dict = dict([(sage_op_dict[k],k) for k in sage_op_dict])
 
 
 ## Here we correct the dictionaries for some simple operators
+
+max_op_dict[mequal] = sage.symbolic.expression.operator.eq
+max_op_dict[mnotequal] = sage.symbolic.expression.operator.ne
 
 def sage_rat(x,y):
     r"""
