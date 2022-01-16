93b877e881fdb63268820732fc6c8f34e5e0e4c7
Frédéric Chapoton <chapoton@unistra.fr>
2022-01-16 10:11:01 +0100

    fix E111 (indentation) in graphs and coding

--- src/sage/coding/delsarte_bounds.py
+++ src/sage/coding/delsarte_bounds.py
@@ -19,15 +19,16 @@ REFERENCES:
 .. [De73] \P. Delsarte, An algebraic approach to the association schemes of coding theory,
     Philips Res. Rep., Suppl., vol. 10, 1973.
 """
-#*****************************************************************************
+# ****************************************************************************
 #       Copyright (C) 2012 Dima Pasechnik <dimpase@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
 
 def krawtchouk(n, q, l, x, check=True):
     r"""
@@ -145,9 +146,9 @@ def _delsarte_LP_building(n, d, d_star, q, isinteger,  solver, maxc = 0):
         rhs = sum([krawtchouk(n,q,j,r,check=False)*A[r] for r in range(n+1)])
         p.add_constraint(0*A[0] <= rhs)
         if j >= d_star:
-          p.add_constraint(0*A[0] <= rhs)
+            p.add_constraint(0*A[0] <= rhs)
         else: # rhs is proportional to j-th weight of the dual code
-          p.add_constraint(0*A[0] == rhs)
+            p.add_constraint(0*A[0] == rhs)
 
     if maxc > 0:
         p.add_constraint(sum([A[r] for r in range(n+1)]), max=maxc)
@@ -241,121 +242,119 @@ def delsarte_bound_hamming_space(n, d, q, return_data=False, solver="PPL", isint
 
 def delsarte_bound_additive_hamming_space(n, d, q, d_star=1, q_base=0,
                      return_data=False, solver="PPL", isinteger=False):
-   """
-   Find a modified Delsarte bound on additive codes in Hamming space ``H_q^n`` of minimal distance ``d``
+    """
+    Find a modified Delsarte bound on additive codes in Hamming space ``H_q^n`` of minimal distance ``d``
 
-   Find the Delsarte LP bound on ``F_{q_base}``-dimension of additive codes in
-   Hamming space ``H_q^n`` of minimal distance ``d`` with minimal distance of the dual
-   code at least ``d_star``.  If ``q_base`` is set to
-   non-zero, then  ``q`` is a power of ``q_base``, and the code is, formally, linear over
-   ``F_{q_base}``. Otherwise it is assumed that ``q_base==q``.
+    Find the Delsarte LP bound on ``F_{q_base}``-dimension of additive codes in
+    Hamming space ``H_q^n`` of minimal distance ``d`` with minimal distance of the dual
+    code at least ``d_star``.  If ``q_base`` is set to
+    non-zero, then  ``q`` is a power of ``q_base``, and the code is, formally, linear over
+    ``F_{q_base}``. Otherwise it is assumed that ``q_base==q``.
 
+    INPUT:
 
-   INPUT:
+    - ``n`` -- the code length
 
-   - ``n`` -- the code length
+    - ``d`` -- the (lower bound on) minimal distance of the code
 
-   - ``d`` -- the (lower bound on) minimal distance of the code
+    - ``q`` -- the size of the alphabet
 
-   - ``q`` -- the size of the alphabet
-
-   - ``d_star`` -- the (lower bound on) minimal distance of the dual code;
+    - ``d_star`` -- the (lower bound on) minimal distance of the dual code;
      only makes sense for additive codes.
 
-   - ``q_base`` -- if ``0``, the code is assumed to be linear. Otherwise,
+    - ``q_base`` -- if ``0``, the code is assumed to be linear. Otherwise,
      ``q=q_base^m`` and the code is linear over ``F_{q_base}``.
 
-   - ``return_data`` -- if ``True``, return a triple ``(W,LP,bound)``, where ``W`` is
-     a weights vector,  and ``LP`` the Delsarte bound LP; both of them are Sage LP
-     data.  ``W`` need not be a weight distribution of a code, or,
-     if ``isinteger==False``, even have integer entries.
+    - ``return_data`` -- if ``True``, return a triple ``(W,LP,bound)``, where ``W`` is
+      a weights vector,  and ``LP`` the Delsarte bound LP; both of them are Sage LP
+      data.  ``W`` need not be a weight distribution of a code, or,
+      if ``isinteger==False``, even have integer entries.
 
-   - ``solver`` -- the LP/ILP solver to be used. Defaults to ``PPL``. It is arbitrary
-     precision, thus there will be no rounding errors. With other solvers
-     (see :class:`MixedIntegerLinearProgram` for the list), you are on your own!
+    - ``solver`` -- the LP/ILP solver to be used. Defaults to ``PPL``. It is arbitrary
+      precision, thus there will be no rounding errors. With other solvers
+      (see :class:`MixedIntegerLinearProgram` for the list), you are on your own!
 
-   - ``isinteger`` -- if ``True``, uses an integer programming solver (ILP), rather
-     that an LP solver. Can be very slow if set to ``True``.
+    - ``isinteger`` -- if ``True``, uses an integer programming solver (ILP), rather
+      that an LP solver. Can be very slow if set to ``True``.
 
-   EXAMPLES:
+    EXAMPLES:
 
-   The bound on dimension of linear `F_2`-codes of length 11 and minimal distance 6::
+    The bound on dimension of linear `F_2`-codes of length 11 and minimal distance 6::
 
-       sage: codes.bounds.delsarte_bound_additive_hamming_space(11, 6, 2)
-       3
-       sage: a,p,val = codes.bounds.delsarte_bound_additive_hamming_space(\
-                            11, 6, 2, return_data=True)
-       sage: [j for i,j in p.get_values(a).items()]
-       [1, 0, 0, 0, 0, 0, 5, 2, 0, 0, 0, 0]
+        sage: codes.bounds.delsarte_bound_additive_hamming_space(11, 6, 2)
+        3
+        sage: a,p,val = codes.bounds.delsarte_bound_additive_hamming_space(
+        ....:                 11, 6, 2, return_data=True)
+        sage: [j for i,j in p.get_values(a).items()]
+        [1, 0, 0, 0, 0, 0, 5, 2, 0, 0, 0, 0]
 
-   The bound on the dimension of linear `F_4`-codes of length 11 and minimal distance 3::
+    The bound on the dimension of linear `F_4`-codes of length 11 and minimal distance 3::
 
-       sage: codes.bounds.delsarte_bound_additive_hamming_space(11,3,4)
-       8
+        sage: codes.bounds.delsarte_bound_additive_hamming_space(11,3,4)
+        8
 
-   The bound on the `F_2`-dimension of additive `F_4`-codes of length 11 and minimal
-   distance 3::
+    The bound on the `F_2`-dimension of additive `F_4`-codes of length 11 and minimal
+    distance 3::
 
-       sage: codes.bounds.delsarte_bound_additive_hamming_space(11,3,4,q_base=2)
-       16
+        sage: codes.bounds.delsarte_bound_additive_hamming_space(11,3,4,q_base=2)
+        16
 
-   Such a ``d_star`` is not possible::
+    Such a ``d_star`` is not possible::
 
-       sage: codes.bounds.delsarte_bound_additive_hamming_space(11,3,4,d_star=9)
-       Solver exception: PPL : There is no feasible solution
-       False
+        sage: codes.bounds.delsarte_bound_additive_hamming_space(11,3,4,d_star=9)
+        Solver exception: PPL : There is no feasible solution
+        False
 
-   TESTS::
+    TESTS::
 
-       sage: a,p,x = codes.bounds.delsarte_bound_additive_hamming_space(\
-                        19,15,7,return_data=True,isinteger=True)
-       sage: [j for i,j in p.get_values(a).items()]
-       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 307, 0, 0, 1, 34]
-       sage: codes.bounds.delsarte_bound_additive_hamming_space(19,15,7,solver='glpk')
-       3
-       sage: codes.bounds.delsarte_bound_additive_hamming_space(\
-                19,15,7, isinteger=True, solver='glpk')
-       3
-   """
-   from sage.numerical.mip import MIPSolverException
-   if q_base == 0:
-      q_base = q
+        sage: a,p,x = codes.bounds.delsarte_bound_additive_hamming_space(
+        ....:           19,15,7,return_data=True,isinteger=True)
+        sage: [j for i,j in p.get_values(a).items()]
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 307, 0, 0, 1, 34]
+        sage: codes.bounds.delsarte_bound_additive_hamming_space(19,15,7,solver='glpk')
+        3
+        sage: codes.bounds.delsarte_bound_additive_hamming_space(
+        ....:    19,15,7, isinteger=True, solver='glpk')
+        3
+    """
+    from sage.numerical.mip import MIPSolverException
+    if q_base == 0:
+        q_base = q
 
-   kk = 0
-   while q_base**kk < q:
-      kk += 1
+    kk = 0
+    while q_base**kk < q:
+        kk += 1
 
-   if q_base**kk != q:
-      print("Wrong q_base=", q_base, " for q=", q, kk)
-      return False
-
-   # this implementation assumes that our LP solver to be unable to do a hot
-   # restart with an adjusted constraint
-
-   m = kk*n # this is to emulate repeat/until block
-   bd = q**n+1
-
-   while q_base**m < bd: # need to solve the LP repeatedly, as this is a new constraint!
-                         # we might become infeasible. More precisely, after rounding down
-                         # to the closest value of q_base^m, the LP, with the constraint that
-                         # the objective function is at most q_base^m,
-      A, p = _delsarte_LP_building(n, d, d_star, q, isinteger,  solver, q_base**m)
-      try:
-        bd=p.solve()
-      except MIPSolverException as exc:
-        print("Solver exception:", exc)
-        if return_data:
-           return A,p,False
+    if q_base**kk != q:
+        print("Wrong q_base=", q_base, " for q=", q, kk)
         return False
+
+    # this implementation assumes that our LP solver to be unable to do a hot
+    # restart with an adjusted constraint
+
+    m = kk*n # this is to emulate repeat/until block
+    bd = q**n+1
+
+    while q_base**m < bd:
+        # need to solve the LP repeatedly, as this is a new constraint!
+        # we might become infeasible. More precisely, after rounding down
+        # to the closest value of q_base^m, the LP, with the constraint that
+        # the objective function is at most q_base^m,
+        A, p = _delsarte_LP_building(n, d, d_star, q, isinteger,  solver, q_base**m)
+        try:
+            bd = p.solve()
+        except MIPSolverException as exc:
+            print("Solver exception:", exc)
+            if return_data:
+                return A, p, False
+            return False
+
     # rounding the bound down to the nearest power of q_base, for q=q_base^m
-#      bd_r = roundres(log(bd, base=q_base))
-      m = -1
-      while q_base**(m+1) < bd:
+    #      bd_r = roundres(log(bd, base=q_base))
+    m = -1
+    while q_base**(m+1) < bd:
         m += 1
-      if q_base**(m+1) == bd:
+    if q_base**(m+1) == bd:
         m += 1
 
-   if return_data:
-      return A, p, m
-   else:
-      return m
+    return A, p, m if return_data else m
